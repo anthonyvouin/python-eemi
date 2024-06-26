fcdb9846f2993d7457af7e7d5a9afa2bda7fb10f
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-06-26 10:31:38 +0200

    add setup api

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from src.controllers.test_controller import router as test_router
 
 app = FastAPI()
+app.include_router(test_router, prefix="/api")
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
