5c09a0c946197c1668a994fe26946f199e2275f2
matthias-evvi <m.faucon@agence-evvi.fr>
2024-06-26 11:51:08 +0200

    fix: update database structure

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from src.config.db import create_database
 from src.controllers.user_controller import router as user_router
 from src.controllers.student_controller import router as student_router
 
-
-
 create_database()
 app = FastAPI()
 app.include_router(user_router, prefix="/api")
